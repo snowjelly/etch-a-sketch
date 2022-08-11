e62cf6a2387d68b303690bdcd70c80de3c19ecf8
snowjelly <broadwellconnor@gmail.com>
2022-08-11 14:08:12 -0500

    getPrevStroke

--- eas.js
+++ eas.js
@@ -6,9 +6,10 @@ let gridSize = 0;
 let gridItemColor = "";
 let eraserSelected = false;
 let strokeComplete = false;
-let strokeBuffer = [];
-let strokeList = [];
+let strokeBuffer = []; //  stores the latest stroke data
+let strokeList = []; // stores stroke history
 let strokeCount = 0;
+let prevStrokeColor = "";
 // quick bugfix to prevent pen always being down
 wrapper.addEventListener('mouseup', () => {
         mousedown = false;
@@ -28,6 +29,7 @@ const createDivs = (gridSize = 16) => {
     newDiv.classList.toggle('grid-item');
     newDiv.classList.toggle(i);
     newDiv.setAttribute('draggable', 'false');
+    newDiv.setAttribute('style', 'background-color: white');
     gridContainer.appendChild(newDiv);
   }
   gridContainer.setAttribute('style', 'grid-template-columns: repeat(' + gridSize + ', auto); grid-template-rows: repeat('+ gridSize +', auto);')
@@ -41,7 +43,12 @@ const getGridSize = (size) => {
 //  strokeBuffer holds a temporary array of the latest stroke colors
 //  strokeList holds an array of every strokeBuffer
 //  i need to gather more data. for example: the position of each gridItem
-// 
+//  autosave when u change grid size. save button. different save for each grid
+//  transfer button or translation button. allows you to translate divs you select or the entire strokeList
+//  transfer button allows you to move your current save across different grids. click it again to stop translating.
+//  you click transfer. you can drag the grid and u can see it scale. you click it again to save to that specific grid size. ignoring the inbetween saves it would normally make
+//  instead of a different save for each slot. maybe just a seperate save slot, creation button, etc.
+//  modular. database for saving. selection tool. rotating. translating
 let mousedown = false;
 const draw = (gridItemColor) => {
   const gridItems = document.querySelectorAll('.grid-item');
@@ -49,20 +56,22 @@ const draw = (gridItemColor) => {
       gridItem.addEventListener('mousedown', () => {
         mousedown = true;
         strokeComplete = false;
+        getPrevStroke(gridItem);
         if (eraserSelected) {
           gridItem.setAttribute('style', 'background-color: white')
         } else {
           gridItem.setAttribute('style', 'background-color: ' + gridItemColor);
         }
-        console.log(getStroke(gridItem));
+        console.log(getStroke(gridItem, prevStrokeColor));
       });
       gridItem.addEventListener('mouseenter', () => {
+        getPrevStroke(gridItem);
         if (mousedown && eraserSelected === false) {  
           gridItem.setAttribute('style', 'background-color: ' + gridItemColor);
-          console.log(getStroke(gridItem));
+          console.log(getStroke(gridItem, prevStrokeColor));
         } else if (mousedown && eraserSelected) {
           gridItem.setAttribute('style', 'background-color: white')
-          console.log(getStroke(gridItem));
+          console.log(getStroke(gridItem, prevStrokeColor));
         } 
         });
       gridItem.addEventListener('mouseup', () => {
@@ -74,7 +83,12 @@ const draw = (gridItemColor) => {
   }); 
 }
 
-  const getStroke = (gridItem) => {
+const getPrevStroke = (gridItem) => {
+  prevStrokeColor = gridItem.getAttribute('style').replace('background-color: ', '');
+  return prevStrokeColor;
+}
+
+  const getStroke = (gridItem, prevStrokeColor) => {
     if (strokeComplete) {
       strokeList.push(strokeBuffer);
       strokeCount++;
@@ -83,10 +97,22 @@ const draw = (gridItemColor) => {
     } else {
       const strokeColor = gridItem.getAttribute('style').replace('background-color: ', '');
       const strokePos = gridItem.className.replace('grid-item ', '');
-      strokeBuffer.push(strokeColor + "-" + strokePos);
+      strokeBuffer.push("was:" + prevStrokeColor + ", " + "is:" + strokeColor + ", " + "at:" + strokePos);
       return strokeBuffer;
     }
   }
+
+const drawHistory = (stroke) => {
+
+}
+
+  //  undo
+  window.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'z') {
+      drawHistory(strokeList[strokeList.length - 1]);
+    }
+  });
+
 
   // eraser button
   const eraser = document.querySelector('.eraser-container button');
